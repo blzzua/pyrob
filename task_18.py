64f76bb7f271ba7b25cb074f652dbb5971290c57
oleg <blzzua@github.com>
2020-02-05 11:08:34 +0200

    solved task_18

--- task_18.py
+++ task_18.py
@@ -5,8 +5,18 @@ from pyrob.api import *
 
 @task
 def task_8_28():
-    pass
-
+    if not wall_is_on_the_left():
+        while wall_is_above():
+            move_left()
+            if wall_is_on_the_left():
+                    while wall_is_above():
+                        move_right()
+    if not wall_is_above():
+        while not wall_is_above():
+            move_up()
+    if not wall_is_on_the_left():
+        while not wall_is_on_the_left():
+            move_left()
 
 if __name__ == '__main__':
     run_tasks()
